7632b2c40290c74eeb2766e08b0626a8dd99b7b3
Éricles Miller <ericlesmiller15@gmail.com>
2022-06-28 14:25:33 -0300

    V2.3

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -16,8 +16,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        score = 0; 
-        scoreText.text = "Score:" + scoreText;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -32,7 +31,9 @@ public class GameManager : MonoBehaviour
              int index = Random.Range(0, targets.Count); //contador do object targets 
              Instantiate(targets[index]);
         }
-        
-
+    }
+    private void UpdateScore(int scoreToAdd){
+        score+= scoreToAdd;
+        scoreText.text = "Score:" + score;
     }
 }
